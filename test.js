a422f66b2966a994f068786b9be71604e2878a55
Venkat Naidu <naiduv@bittorrent.com>
2012-07-07 13:01:37 -0700

    fix the bug causing it to slow down after a few uses

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var bkcolor = "#FFFFFF"
 //this is your timer, does nothing right now
 var ls;
 
@@ -14,20 +15,21 @@ var timer;
 var ammo = 0;
 
 soldier = function(x, y){
+	this.selected = false;
 	this.lookx= 0;
 	this.looky = 0;
 	this.lookangle = 0;
+	this.w = 30;
+	this.h = 34;
 	//this.element = elem;
-	this.xloc = x;
-	this.yloc = y;
-
+	this.loc = new Point(x,y);
+	this.rect = new Rect(x-this.w/2,y+this.h/2,this.w,this.h); //scaled because of translation for rotation, see draw()
 	this.ywalkloc = 1;
 	this.ywalkcycle = [	"soldier_walking_1", 
 						"soldier_walking_2",
 						"soldier_walking_3"];
 	this.firing = false;
 	this.ammo = 10;
-	_this = this;
 }
 
 var increment = 10;
@@ -35,63 +37,68 @@ var increment = 10;
 soldier.prototype = {
 	draw : function(){
 		// ctx.fillStyle="#FFFFFF";
-		// ctx.fillRect(_this.xloc-10,_this.yloc-10,60,60);
+		// ctx.fillRect(this.loc.x-10,this.loc.y-10,60,60);
 		ctx.save(); // save current state
-		ctx.translate(_this.xloc, _this.yloc);
-		ctx.translate(15,17);
-    	ctx.rotate(_this.lookangle*Math.PI/180); // rotate
-    	ctx.fillStyle="#FFFFFF";
-		ctx.arc(0,0,40,0,2*Math.PI,false);
-		ctx.fill();
-		if(_this.firing) {
-    		ctx.drawImage(document.getElementById("gunfire"),-4,-28,15,12);
-    	}
-		ctx.drawImage(document.getElementById(_this.ywalkcycle[_this.ywalkloc]),-15,-17,30,34);
+		//use the height because it is longer (in the next line)
+		ctx.clearRect(this.loc.x-this.w,this.loc.y-this.h,this.w*2,this.h*2);
+		ctx.translate(this.loc.x, this.loc.y);
+		//ctx.translate(this.w/2,this.h/2);
+ 		ctx.rotate(this.lookangle*Math.PI/180); // rotate
+ 		//debugger;
+		if(this.selected){
+			ctx.arc(0,0,40,0,2*Math.PI,false);
+			ctx.stroke();
+		}
+
+		if(this.firing)
+    		ctx.drawImage(document.getElementById("gunfire"),-4,-28,this.w/2,this.h/2);
+		
+		ctx.drawImage(document.getElementById(this.ywalkcycle[this.ywalkloc]),
+			-this.w/2,-this.h/2,this.w,this.h);
+		
 		ctx.restore();
 	},
 
 	moveup : function(){
-		_this.xloc += increment*Math.sin(_this.lookangle*Math.PI/180);
-		_this.yloc -= increment*Math.cos(_this.lookangle*Math.PI/180);
-		_this.draw();
+		this.loc.x += increment*Math.sin(this.lookangle*Math.PI/180);
+		this.loc.y -= increment*Math.cos(this.lookangle*Math.PI/180);
+		this.draw();
 	},
 
 	movedown : function(){
-		_this.xloc -= increment*Math.sin(_this.lookangle*Math.PI/180);
-		_this.yloc += increment*Math.cos(_this.lookangle*Math.PI/180);	
-		_this.draw();
+		this.loc.x -= increment*Math.sin(this.lookangle*Math.PI/180);
+		this.loc.y += increment*Math.cos(this.lookangle*Math.PI/180);	
+		this.draw();
 	},
 
 	fire: function(){
-		if(_this.ammo<=0)
+		if(this.ammo<=0)
 			return;
 
-		_this.ammo--;
-		_this.firing = true;
-		_this.draw();
+		this.ammo--;
+		this.firing = true;
+		this.draw();
 
-		ctx.fillStyle="#FFFFFF";
+		ctx.fillStyle=bkcolor;
 		ctx.fillRect(200,10,70,15);
-		ctx.strokeText("ammo left - "+_this.ammo, 200, 20);
+		ctx.strokeText("ammo left - "+this.ammo, 200, 20);
 
 		//this is a hack to delete the gunfire image..
-		_this.firing = false;
+		this.firing = false;
 	}
 }
 
-window.onkeyup = function(e){
+window.onkeydown = function(e){
 	if(ls==undefined)
 		return;
+
+	console.log('keyup');
 	
-	var evtobj=window.event? event : e;
-	var unicode=evtobj.charCode? evtobj.charCode : evtobj.keyCode;
-	var actualkey=String.fromCharCode(unicode);
-	
-	switch(actualkey) {
-		case "W":
+	switch(e.keyCode) {
+		case 87:
 			ls.moveup();
 			break;
-		case "S":
+		case 83:
 			ls.movedown();
 			break;
 	}
@@ -101,10 +108,18 @@ window.onmousedown = function(e){
 	if(ls==undefined)
 		return;
 
+	mpt = new Point(e.pageX, e.pageY);
+	if(ptinrect(mpt,ls.rect)){
+		ls.selected = true;
+		return;
+	}
+
 	ls.fire();
 	console.log("fire");
 	return;
 }
+
+
 
 window.onmousemove = function(e){
 	if(ls==undefined)
@@ -113,8 +128,8 @@ window.onmousemove = function(e){
 	ls.lookx = e.pageX;
 	ls.looky = e.pageY;
 
-	disty = ls.yloc - ls.looky;
-	distx = ls.xloc - ls.lookx;
+	disty = ls.loc.y - ls.looky;
+	distx = ls.loc.x - ls.lookx;
 	hyp = Math.sqrt(disty*disty + distx*distx);
 	ls.lookangle =Math.atan2(disty,distx) * 180/Math.PI; 
 	if (ls.lookangle < 0) ls.lookangle += 360;
@@ -133,10 +148,11 @@ window.onload = function() {
 	c = document.getElementById("myCanvas");
 	ctx = c.getContext("2d");
 
+	ctx.fillStyle=bkcolor;
+	ctx.fillRect(0,0,500,500);
+
 	while(soldier_count<1) {
-		ctx.fillStyle="#FFFFFF";
-		ctx.fillRect(0,0,500,500);
-		ls = new soldier(200,200);
+		ls = new soldier(200-(soldier_count*100),200);
 		ls.draw();
 		soldier_count++;
 	}
