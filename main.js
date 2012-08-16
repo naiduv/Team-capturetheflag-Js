1885772ba291de6103e1c56880c0e33ae76e9cf5
Venkat Naidu <naiduv@bittorrent.com>
2012-08-16 16:36:16 -0700

    wip

--- main.js
+++ main.js
@@ -8,175 +8,14 @@ var canvas_h;
 var stop_running = false;
 var fb;
 var fbgameref;
-
-//randomly returns a +1 or -1
-randfunc = function(){
-	if(Math.random()<0.5) 
-		return 1; 
-	else 
-		return -1;
-}
-
+var ctx0;
+var ctx1;
 var soldier_count = 0;
 var timer;
 var ammo = 0;
 var teamid = Math.floor(Math.random()*10000);
 var idcount = 0;
-
-soldier = function(x, y){
-	this.selected = false;
-	this.id = Math.floor(Math.random()*10000);
-	this.teamid = teamid;
-	this.lookpt = makepoint(myrand(100),myrand(100));
-	this.lookangle = 0;
-	this.w = 25;
-	this.h = 29;
-	//this.element = elem;
-	this.loc = new Point(x,y);
-	this.rect = new Rect(this.loc.x-this.w/2-increment*2,this.loc.y-this.h/2-increment*2,this.w+(4*increment),this.h+(4*increment));
-	this.ywalkloc = 1;
-	this.ywalkcycle = [	"soldier_walking_1", 
-						"soldier_walking_2",
-						"soldier_walking_3"];
-	this.firing = false;
-	this.ammo = 100;
-	this.health = 100;
-	this.alive = true;
-	this.bleeding = false;
-	this.waypoint = [];
-	_this = this;
-}
-
-makesoldier = function(teamid, id,x,y,lookx,looky) {
-	s = new soldier(x,y);
-	s.teamid = teamid;
-	s.id = id;
-	s.lookpt = makepoint(lookx, looky);
-	return s;
-}
-
 var increment = 4;
-
-soldier.prototype = {
-	draw : function(movesoldier){
-		ctx.save(); // save current state
-		this.rect.clear(ctx);
-
-		ctx.translate(this.loc.x, this.loc.y);
- 		ctx.rotate(this.lookangle*Math.PI/180); // rotate
-
-		ctx.drawImage(document.getElementById(this.walkingimage(movesoldier)),
-			-this.w/2,-this.h/2,this.w,this.h); //save the returns of document.getelementbyid in variables! should be faster
-		
-		if(this.firing)
-    		ctx.drawImage(document.getElementById("gunfire"),0,-19,this.w/3,this.h/3);
-
-    	if(this.bleeding)
-    		ctx.drawImage(document.getElementById("bleeding"),0,0,this.w/1,this.h/1);
-
-		ctx.restore();
-	},
-
-	walkingimage: function(movesoldier){
-		if(movesoldier) {		
-			this.ywalkloc++;
-			if(this.ywalkloc==3)
-				this.ywalkloc=0;
-		}
-		return this.ywalkcycle[this.ywalkloc];
-	},
-
-	moveup : function(force){
-		this.loc.x += increment*Math.sin(this.lookangle*Math.PI/180);
-		this.loc.y -= increment*Math.cos(this.lookangle*Math.PI/180);
-    	this.rect = makerect(this.loc.x-this.w/2-increment*2,this.loc.y-this.h/2-increment*2,this.w+(4*increment),this.h+(4*increment));
-    	
-    	//forcing it down if it collides with another rect
-		if(!force) {
-    		for (var i in soldiers) {
-    			if(this.id!=soldiers[i].id && soldiers[i].alive && rectscollide(this.rect, soldiers[i].rect)) {
-					this.movedown(true);
-    				return;
-    			}
-    		}
-    	}
-   		this.draw(true);
-	},
-
-	movedown : function(force){
-		this.loc.x -= increment*Math.sin(this.lookangle*Math.PI/180);
-		this.loc.y += increment*Math.cos(this.lookangle*Math.PI/180);	
-		this.rect = makerect(this.loc.x-this.w/2-increment*2,this.loc.y-this.h/2-increment*2,this.w+(4*increment),this.h+(4*increment));
-
-		//forcing it up if it collides with another rect
-		if(!force) {
-    		for (var i in soldiers) {
-    			if(this.id!=soldiers[i].id && rectscollide(this.rect, soldiers[i].rect)) {
-    				this.moveup(true);
-    				return;
-    			}
-    		}
-    	}
-		this.draw(true);
-	},
-
-	fire: function(){
-		if(this.ammo<=0)
-			return;
-
-		this.ammo--;
-		this.firing=true;
-		this.draw();
-
-		setTimeout(function(_this) {_this.hidefire();},100,this);
-	},
-
-	hidefire: function(){
-		this.firing=false;
-		this.draw();
-	},
-
-	hit: function(damage){
-		console.log('hit');
-		this.health -= damage;
-		
-		this.bleeding = true;
-
-		this.draw();
-
-		if(this.health<=0)
-			this.kill();
-		else
-			setTimeout(function(_this) {_this.stopbleeding();},50,this);
-	},
-
-	stopbleeding: function(){
-		this.bleeding = false;
-		if(this.alive) this.draw();
-	},
-
-	kill: function(){
-		this.alive = false;
-		this.loc.x = 10000; //hack to make it go away
-		this.rect.clear(ctx);
-	},
-
-	lookat: function(pt){
-		this.lookpt = pt;
-		this.calclookangle();
-	},
-
-	calclookangle: function(){
-		distx = this.loc.x - this.lookpt.x;
-		disty = this.loc.y - this.lookpt.y;
-		hyp = Math.sqrt(disty*disty + distx*distx);
-	
-		this.lookangle =Math.atan2(disty,distx) * 180/Math.PI; 
-		if (this.lookangle < 0) this.lookangle += 360;
-		this.lookangle -=90;
-	}
-
-}
 
 window.onkeydown = function(e){
 	if(ls==0)
@@ -272,17 +111,17 @@ function handleclick(pt){
 	if(wassoldierselected && !newsoldierselected) {
 		console.log('waypoint added')
 		ls.waypoint.push(pt);
-		//ctx.fillStyle="#FF0000";
+		//ctx0.fillStyle="#FF0000";
 		//ls = 0;
 	}
 }
 
 window.onresize = function()
 {
-  c = document.getElementById("myCanvas");
-  ctx = c.getContext("2d");
-  ctx.canvas.width  = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
+  var c = document.getElementById("top-canvas");
+  ctx0 = c.getContext("2d");
+  ctx0.canvas.width  = window.innerWidth;
+  ctx0.canvas.height = window.innerHeight;
 }
 
 function canvasmousemove(e){
@@ -295,7 +134,7 @@ function canvasmousemove(e){
 	ls.lookpt.x = e.layerX + 0.1*e.layerX; //90% css
 	ls.lookpt.y = e.layerY + 0.1*e.layerY;
 
-	//ctx.fillRect(ls.lookx, ls.looky, 2, 2);
+	//ctx0.fillRect(ls.lookx, ls.looky, 2, 2);
 	ls.calclookangle();
 	ls.draw();
 }
@@ -333,7 +172,7 @@ function commandloop() {
 			//when we get to a waypoint
 			if(ptinrect(soldiers[i].waypoint[0], soldiers[i].rect)) {
 				console.log('hit waypoint');
-				//ctx.clearRect(soldiers[i].waypoint.x, soldiers[i].waypoint.y,5,5);
+				//ctx0.clearRect(soldiers[i].waypoint.x, soldiers[i].waypoint.y,5,5);
 				//remove the waypoint
 				soldiers[i].waypoint.shift();
 				//point the soldier in the new waypoint dir, if it exists
@@ -417,14 +256,16 @@ function randomloop()
 }
 
 function initcanvas(){
-	c = document.getElementById("myCanvas");
+	var c = document.getElementById("top-canvas");
+ 	ctx0 = c.getContext("2d");
+  	canvas_w = ctx0.canvas.width  = window.innerWidth;
+  	canvas_h = ctx0.canvas.height = window.innerHeight;
 	c.addEventListener('mousemove', canvasmousemove);
 	c.addEventListener('mouseup', canvasmouseup);
 	c.addEventListener('dblclick', canvasdblclick);
 
- 	ctx = c.getContext("2d");
-  	canvas_w = ctx.canvas.width  = window.innerWidth;
-  	canvas_h = ctx.canvas.height = window.innerHeight;
+	// c = document.getElementById("bottom-canvas");
+	//  = c.getContext("2d");
 }
 
 //initialize the units
