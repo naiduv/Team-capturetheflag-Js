29792e9eaf135ede6c9949927710af56ed0afe6d
naiduv <naiduv@bittorrent.com>
2012-08-25 09:43:55 -0700

    wip

--- serve.cpp
+++ serve.cpp
@@ -10,20 +10,54 @@
 #include "./base64.h"
 #include <pthread.h>
 #include <unistd.h>
+#include <assert.h>
 
 using namespace std;
 socklen_t sockfd, newsockfd, portno, clilen;
+bool g_force_exit = false;
+#define MAX_GAMES 10
+#define MAX_SOCKETS 25
+
+int g_numgames = 0;
+
+class socketlist
+{
+private:
+  socklen_t list[MAX_SOCKETS];
+  int num_sockets;
+public:
+  void addsocket(socklen_t newsocket){
+    list[num_sockets]=newsocket;
+    num_sockets++;
+  }
+  
+  int size(){
+    return num_sockets;
+  }
+
+  socklen_t getsock(int item){
+    return list[item];
+  }
+
+  void removesocket(socklen_t newsocket){
+    cout<<"\n implement me list.removesocket()";
+    assert(0);
+  }
+
+  socketlist():num_sockets(0){}
+
+}g_socketlist;
 
 class game
 {
 private:
   //each game has 2 sockets
-  socklen_t *_sockets[2];
+  socklen_t _sockets[2];
   string _gameid;
   int _num_socks; 
 public:
   game(string gameid):_gameid(gameid),_num_socks(0) {}
-  void addsocket(socklen_t *sock) { 
+  void addsocket(socklen_t sock) { 
     if(_num_socks==2) {
       cout<<"\n game is full";
       return;
@@ -33,16 +67,10 @@ public:
   }
 };
 
-#define MAX_GAMES 10
-#define MAX_SOCKETS 25
-game *gamelist[MAX_GAMES];
-socklen_t socketlist[MAX_SOCKETS];
-int g_numgames = 0;
-
 void creategame(string gameid)
 {
   game *new_game = new game(gameid);
-  gamelist[g_numgames] = new_game;
+  //  gamelist[g_numgames] = new_game;
   g_numgames++;
   cout<<"\n new game created";
 }
@@ -63,13 +91,13 @@ int main( int argc, char *argv[] )
   int listenret = pthread_create(&listen_thread, NULL, listen_loop, (void*)NULL);
   int readkbret = pthread_create(&read_keyboard_thread, NULL, read_keyboard_loop, (void*)NULL);
   int closesockret = pthread_create(&close_socks_thread, NULL, close_socks_loop, (void*)NULL);
-  //  int recvret = pthread_create(&recv_thread, NULL, recv_loop, (void*)NULL);
+  int recvret = pthread_create(&recv_thread, NULL, recv_loop, (void*)NULL);
   cout<<"\n thread create completed";
 
   pthread_join(listen_thread, NULL);
   pthread_join(read_keyboard_thread, NULL);
   pthread_join(close_socks_thread, NULL);
-  //  pthread_join(recv_thread, NULL);
+  pthread_join(recv_thread, NULL);
 }
 
 void send_msg()
@@ -84,7 +112,6 @@ void send_msg()
   send(newsockfd, buffer, strlen(buffer), 0);
 }
 
-bool g_force_exit = false;
 void* read_keyboard_loop(void *ptr)
 {
   cout<<"\n entering read keyb loop";
@@ -107,38 +134,40 @@ void* recv_loop(void *ptr)
   
   while(!g_force_exit){
     
-    if(newsockfd){
+    for(int i=0; i<g_socketlist.size();i++){
+      socklen_t sock = g_socketlist.getsock(i); 
       char rb[10];
       
       //byte 0 -> fin, opcode
-      recv(newsockfd, rb, 1, 0);
+      recv(sock, rb, 1, 0);
       bool fin = (bool)(*rb & 0x80);
-      cout<<"\n fin: "<<fin;
+      //cout<<"\n fin: "<<fin;
      
       char opcode = *rb & 0x0F;
       
-      if(opcode == 0x01)
-        cout<<"\n opcode: text frame";
-      else if(opcode == 0x08)
-        cout<<"\n opcode: connection close";
-      else {
-        cout<<"\n opcode: not handled";
+      if(opcode == 0x01){
+        //cout<<"\n opcode: text frame";
+      }else if(opcode == 0x08){
+        //cout<<"\n opcode: connection close";
+	continue;
+      } else {
+        //cout<<"\n opcode: not handled";
 	continue;
       }
 
       //byte 1 -> ismask, payload len
-      recv(newsockfd, rb, 1, 0);
+      recv(sock, rb, 1, 0);
       int length = *rb & 0x7F;
-      cout<<"\n length: "<<length;
+      //cout<<"\n length: "<<length;
 
       //byte 2-9 -> check 2-7 for extended payload
       //recv(newsockfd, rb, 8, 0);
 
-      cout<<"\n getting mask\n";
+      //cout<<"\n getting mask\n";
       //byte 10-13 -> masking
       char mask[4];
       for(int i=0; i<4;i++){
-	recv(newsockfd, rb, 1, 0);
+	recv(sock, rb, 1, 0);
 	mask[i] = *rb;
       }
  
@@ -148,7 +177,7 @@ void* recv_loop(void *ptr)
       int data;
       char str[100];
       for (int i = 0; i < length; i++) {
-        recv(newsockfd, rb, 1, 0);
+        recv(sock, rb, 1, 0);
 	//cout<<"\n payload rb: "<<(*rb^mask[i%4]);
         data = (*rb ^ mask[i % 4]);
 	//cout<<"\n data: "<<data;
@@ -159,7 +188,7 @@ void* recv_loop(void *ptr)
       cout<<"\n datastr: "<<str;
       creategame(str);
       
-      cout<<"\n read complete\n\n";
+      //cout<<"\n read complete\n\n";
     }
   }
    
@@ -190,9 +219,13 @@ void* listen_loop(void *ptr)
  
   /* Now bind the host address using bind() call.*/
   int optval = 1;
+  timeval tval;
+  tval.tv_sec = 0;
+  tval.tv_usec = 100; //this introduces latency
+
   setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-  if (bind(sockfd, (struct sockaddr *) &serv_addr,
-	   sizeof(serv_addr)) < 0)
+  setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tval, sizeof(tval));
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
     {
       cout<<"\nERROR on binding";
       return(0);
@@ -201,10 +234,9 @@ void* listen_loop(void *ptr)
   /* Now start listening for the clients, here process will
    * go in sleep mode and will wait for the incoming connection
    */
-  while(1) {
-    cout<<"\n***New listen cycle***\n";
-    
+  while(!g_force_exit) {
     listen(sockfd,5);
+
     clilen = sizeof(cli_addr);
 
     /* Accept actual connection from the client */
@@ -213,17 +245,19 @@ void* listen_loop(void *ptr)
 
     if (newsockfd < 0) 
       {
-	cout<<"\nERROR on accept";
-	return(0);
+	//cout<<"\nERROR on accept";
+	continue;
       }
 
     /* If connection is established then start communicating */
     memset(buffer, 0, sizeof(buffer));
     n = read(newsockfd,buffer,sizeof(buffer));
     if (n < 0){
-      cout<<"\nERROR reading from socket";
+      //cout<<"\nERROR reading from socket";
       continue;
     }
+
+    cout<<"\n Got Connection";
 
     if(strlen(buffer)==0) {
       cout<<"\n buffer empty";
@@ -275,6 +309,9 @@ void* listen_loop(void *ptr)
       cout<<"\nERROR writing to socket";
       return(0);
     }
+
+    g_socketlist.addsocket(newsockfd);
+
   }
   return 0; 
 }
